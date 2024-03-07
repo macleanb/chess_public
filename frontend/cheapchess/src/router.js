f3f6b3d31811e9224a98aa4a193ef64ecb8cc34d
Brian MacLean <brian_maclean@hotmail.com>
2024-03-07 07:02:53 -0600

    updated component filenames

--- frontend/cheapchess/src/router.js
+++ frontend/cheapchess/src/router.js
@@ -3,7 +3,7 @@ import { createBrowserRouter } from 'react-router-dom';
 
 /* Internal Imports */
 import App from './App';
-import Game from './pages/game';
+import Game from './pages/Game';
 
 const router = createBrowserRouter([
   {
