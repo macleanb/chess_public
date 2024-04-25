9e938a4c71b76363acb1d02f2e8a282dd6c96f4c
CHRIS <chsbca999@gmail.com>
2024-04-25 10:43:44 -0700

    cleaned out commented code. Also added sort mapping function to allow games to be ordered by game id in ascending order

--- frontend/cheapchess/src/components/ListOfPlayableGames.js
+++ frontend/cheapchess/src/components/ListOfPlayableGames.js
@@ -1,50 +1,23 @@
-// import { useState, useEffect, useContext } from "react";
 import { useState } from "react";
-// import axios from "axios";
-// import AuthContext from "../contexts/AuthProvider";
-// import constants from "../constants";
 import { Button, Offcanvas } from "react-bootstrap";
 
 const ListOfPlayableGames = ({ parentState }) => {
-    // const [listOfGames, setListOfGames] = useState([])
-    const [show, setShow] = useState(false)
-    // const { auth } = useContext(AuthContext)
-
-    const handleClose = () => setShow(false)
-    const handleShow = () => setShow(true)
-
-    // useEffect(() => {
-    //     if (auth.status === constants.STATUS_AUTHENTICATED) {
-    //         // fetchPlayableGames()
-    //     }
-    // }, [auth.status]);
-
-    // const fetchPlayableGames = async () => {
-    //     try {
-    //         const response = await axios.get('/games/playable/', {
-    //             withCredentials: true
-    //         })
-    //         const sortedGames = response.data.sort((a, b) => a.id - b.id);
-    //         setListOfGames(sortedGames)
-    //     } catch (error) {
-    //         console.error('Failed to fetch playable games:', error)
-    //     }
-    // }
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     return (
         <>
             <Button variant="primary" onClick={handleShow}>
                 Show Playable Games
             </Button>
-
             <Offcanvas show={show} onHide={handleClose}>
                 <Offcanvas.Header closeButton>
                     <Offcanvas.Title>List of Playable Games</Offcanvas.Title>
                 </Offcanvas.Header>
                 <Offcanvas.Body>
                     <ul>
-                        {/* {listOfGames.map(game => ( */}
-                        {parentState?.playableGames?.map(game => (
+                        {parentState?.playableGames?.sort((a, b) => a.id - b.id).map(game => (
                             <li key={game.id}>
                                 Game #{game.id} <Button variant="primary" onClick={() => console.log("Joining game #", game.id)}>
                                     Join
