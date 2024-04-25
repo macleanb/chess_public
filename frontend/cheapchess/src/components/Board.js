ca90c4e26a636726f7629152b5ad50186721d55e
Brian MacLean <brian_maclean@hotmail.com>
2024-04-25 05:23:47 -0500

    updated to correctly handle square highlighting with complex async scenarios including multiple rapid and successive user clicks

--- frontend/cheapchess/src/components/Board.js
+++ frontend/cheapchess/src/components/Board.js
@@ -1,10 +1,10 @@
-/* This component represents a chessboard */
-
 /* External Imports */
+import { useEffect, useState } from 'react';
 
 /* Internal Imports */
 import makeMove from "../utils/board_MakeMove";
-   
+
+/* This component represents a chessboard */
 const Board = (
   {
     parentState
@@ -13,6 +13,9 @@ const Board = (
   ////////////////////
   /// Declarations ///
   ////////////////////
+
+  /* Store possible moves once received from API */
+  const [ possibleMoves, setPossibleMoves ] = useState();
 
   //////////////////////
   /// Event Handlers ///
@@ -25,79 +28,16 @@ const Board = (
     // e.stopPropagation();
   };
 
-  /* bring in the state from game.js parent state*/
-  const { selectedOriginSquare, setSelectedOriginSquare } = parentState;
-
   /* Handle click behavior for square components */
   const handleSquareClicked = async (e, squareData) => {
     e.preventDefault();
     e.stopPropagation();
-    console.log(squareData)
 
-    if (selectedOriginSquare === squareData){
-      setSelectedOriginSquare(null); //deselect the square if its already selected
+    if (parentState.selectedOriginSquare === squareData){
+      parentState.setSelectedOriginSquare(null); //deselect the square if its already selected
     }
     else{
-      setSelectedOriginSquare(squareData);//select the square
-    }
-
-    /* Get the piece data from the square */
-    const pieceData = squareData.piece;
-
-    if (pieceData && parentState?.imports?.apiGetPossibleMoves) {
-      const response = await parentState.imports.apiGetPossibleMoves(
-        pieceData.color,
-        pieceData.first_move_made,
-        pieceData.current_file,
-        pieceData.current_rank,
-        pieceData.piece_type
-      );
-
-      /* Restore any highlighted squares to their original color */
-      const tempBoardData = [...parentState.boardData]; // copy boardData
-      for (const highlightedSquare of parentState.highlightedSquares) {
-        const squareData = parentState.imports.getSquareData(
-          tempBoardData,
-          highlightedSquare.square,
-          parentState.playerColor
-        );
-        squareData.color = highlightedSquare.originalColor;
-      }
-
-      /* For each square in the response (possible moves),
-         color the square on our board green IF there isn't
-         already a piece on it */
-      const newHighlightedSquares = [];
-      const selectedPiecesSquare = pieceData.current_file + pieceData.current_rank;
-      for (const responseSquare of response) {
-        const boardSquareData = parentState.imports.getSquareData(
-          tempBoardData,
-          responseSquare,
-          parentState.playerColor
-          );
-
-        /* Make sure there is no piece before highlighting */
-        if (
-          boardSquareData.piece === null &&
-          !parentState.imports.pieceExistsBetweenTwoSquares(
-            tempBoardData,
-            selectedPiecesSquare,
-            responseSquare,
-            parentState.playerColor
-            )
-          ) {
-          const originalSquareColor = boardSquareData.color;
-          boardSquareData.color = 'greensquare';
-  
-          newHighlightedSquares.push({
-            square         : responseSquare,
-            originalColor  : originalSquareColor
-          });
-        }
-      }
-
-      parentState.setBoardData(tempBoardData);
-      parentState.setHighlightedSquares(newHighlightedSquares);
+      parentState.setSelectedOriginSquare(squareData);//select the square
     }
 
     // test/dev
@@ -106,7 +46,7 @@ const Board = (
     const moveIsValid = parentState.imports.isValidMove(
       squareData
     );
-    console.log(`Here in Board.js.  The move ${moveIsValid ? 'is' : 'is not'} valid!`);
+    //console.log(`Here in Board.js.  The move ${moveIsValid ? 'is' : 'is not'} valid!`);
 
     // test/dev only
     // console.log('Piece or square was clicked!');
@@ -126,6 +66,125 @@ const Board = (
   ///////////////////
   /// Use Effects ///
   ///////////////////
+
+  /* Whenever selectedOriginSquare changes, update possible moves */
+  useEffect(() => {
+    /* Only reset highlighted squares if parentState.boardData is not null */
+    if (parentState.boardData) {
+      /* Restore any highlighted squares to their original color.  This
+       is needed for both square deselection and selecting a different
+       square */
+      const tempBoardData = [...parentState.boardData]; // copy boardData
+      for (const highlightedSquare of parentState.highlightedSquares) {
+        const squareData = parentState.imports.getSquareData(
+          tempBoardData,
+          highlightedSquare.square,
+          parentState.playerColor
+        );
+        squareData.color = highlightedSquare.originalColor;
+      }
+
+      parentState.setBoardData(tempBoardData);
+      parentState.setHighlightedSquares([]);
+    }
+  
+    /* If a square has been selected, get possible moves for that square */
+    if (
+      parentState.selectedOriginSquare &&
+      parentState.selectedOriginSquare.piece &&
+      parentState?.imports?.apiGetPossibleMoves
+      ) 
+    {
+      /* Get the piece data from the selected square */
+      const pieceData = parentState.selectedOriginSquare.piece;
+
+      /* Set local state to track the reference square that possible
+          moves will be requested for */
+      const referenceOriginSquare = parentState.selectedOriginSquare;
+
+      /* Create 'active' variable for clean-up up purposes */
+      let active = true;
+
+      const getPossibleMoves = async () => {
+        const newPossibleMoves = await parentState.imports.apiGetPossibleMoves(
+          pieceData.color,
+          pieceData.first_move_made,
+          pieceData.current_file,
+          pieceData.current_rank,
+          pieceData.piece_type
+        );
+
+        if (active && newPossibleMoves) {
+          setPossibleMoves({
+            referenceOriginSquare : referenceOriginSquare,
+            moves : newPossibleMoves
+          });
+        }
+      };
+
+      getPossibleMoves();
+    } else {
+      /* If selectedOriginSquare is null or if there is no piece on
+         the selected square, set possibleMoves to null */
+      setPossibleMoves(null);
+    }
+  }, [parentState?.selectedOriginSquare]);
+
+  /* Whenever possibleMoves changes, highlight appropriate squares */
+  useEffect(() => {
+    /* Check to ensure the referenceSquare hasn't been deselected by
+        the time a promise resolves or selected to a different square.
+        If it has been deselected or changed, do nothing. */
+    if (
+      parentState.boardData &&
+      possibleMoves &&
+      parentState.selectedOriginSquare &&
+      parentState.selectedOriginSquare === possibleMoves.referenceOriginSquare
+      )
+    {
+      /* Copy boardData before updating state */
+      const tempBoardData = [...parentState.boardData];
+
+      /* Get the piece data from the selected square */
+      const pieceData = parentState.selectedOriginSquare.piece;
+
+      /* For each square in possible moves, color the square on our
+      board green IF there isn't already a piece on it */
+      const newHighlightedSquares = [];
+      const selectedPiecesSquare = pieceData.current_file + pieceData.current_rank;
+      for (const possibleMoveSquare of possibleMoves.moves) {
+        const possibleMoveSquareData = parentState.imports.getSquareData(
+          tempBoardData,
+          possibleMoveSquare,
+          parentState.playerColor
+          );
+
+        /* Make sure there is no piece before highlighting */
+        if (
+          possibleMoveSquareData.piece === null &&
+          !parentState.imports.pieceExistsBetweenTwoSquares(
+            tempBoardData,
+            selectedPiecesSquare,
+            possibleMoveSquare,
+            parentState.playerColor
+            )
+          )
+        {
+          const originalSquareColor = possibleMoveSquareData.color;
+          possibleMoveSquareData.color = 'greensquare';
+  
+          newHighlightedSquares.push({
+            square         : possibleMoveSquare,
+            originalColor  : originalSquareColor
+          });
+        }
+      }
+
+      /* Keep track of highlighted squares for constant-time lookup */
+      parentState.setHighlightedSquares(newHighlightedSquares);
+      parentState.setBoardData(tempBoardData);
+    }
+  }, [possibleMoves]);
 
   //////////////
   /// Render ///
@@ -151,8 +210,7 @@ const Board = (
                                 ...parentState,
                                 squareData          :   squareData,
                                 handlePieceClicked  :   handlePieceClicked,
-                                handleSquareClicked :   handleSquareClicked,
-                                isSelected          :   selectedOriginSquare === squareData.position
+                                handleSquareClicked :   handleSquareClicked
                               }}/>
                             </parentState.imports.Col>
                           )
